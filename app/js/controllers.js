d0be8d977b6b3856d083410197d40a8263ecff8f
Tomer Simis <tdl.simis@gmail.com>
2015-05-01 21:24:39 -0300

    Created a function to set tasks done

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,9 +2,15 @@ var getDoneControllers = angular.module('getDoneControllers', []);
 
 getDoneControllers.controller('TaskList', function($scope){
 	$scope.tasks = [];
+	$scope.currentId = 0;
 
 	$scope.createTask = function(){
-		$scope.tasks.push({'description': $scope.description, 'date' : new Date()});
+		$scope.tasks.push({'id' : $scope.currentId,'description': $scope.description, 'date' : new Date(), 'done' : false});
 		$scope.description = "";
+		$scope.currentId++;
+	}
+
+	$scope.setDone = function(id){
+		$scope.tasks[id].done = !$scope.tasks[id].done;
 	}
 })
